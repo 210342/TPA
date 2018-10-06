58b36db156db9577fdcff9fbffb71c0fd0713ac7
Adam Troszczyński <210342@edu.p.lodz.pl>
2018-10-06 11:45:15 +0200

    Initial commit

---
+++ CommandLineInterface/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineInterface
+{
+    class Startup
+    {
+        private readonly string _filename;
+        static void Main(string[] args)
+        {
+            if(args.Count() > 0)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Please give a correct path to a .dll");
+            }
+        }
+    }
+}
