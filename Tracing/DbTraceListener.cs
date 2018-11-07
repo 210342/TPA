dea6e520db7e2bd8300cdce1152fd74a911c8fc3
Adrian Fijałkowski <fijaladrian@gmail.com>
2018-11-07 12:22:08 +0100

    DbTrace listeneer, added option to change table content

--- Tracing/DbTraceListener.cs
+++ Tracing/DbTraceListener.cs
@@ -9,15 +9,41 @@ namespace Tracing
 {
     public class DbTraceListener : TraceListener
     {
-        private string connectionString;
-        private readonly StringBuilder queriesBuilder = new StringBuilder();
-        DatabaseHandling.IDatabaseWriter databaseWriter;
-
+        #region private fields
         private readonly Dictionary<string, string> acceptedFields = 
             new Dictionary<string, string>() { { "messageField", "log_message" }, { "time", "log_time" } };
-        private readonly string tableName = "tracing_logs";
-        private readonly string dbName = "AppTracing";
+        private string connectionString;
+        private readonly StringBuilder queriesBuilder = new StringBuilder();
+        private DatabaseHandling.IDatabaseWriter databaseWriter;
+        #endregion
+        #region properties
+        public string MessageField
+        {
+            get
+            {
+                return acceptedFields["messageField"];
+            }
+            set
+            {
+                acceptedFields["messageField"] = value;
+            }
+        }
+        public string TimeField
+        {
+            get
+            {
+                return acceptedFields["time"];
+            }
+            set
+            {
+                acceptedFields["time"] = value;
+            }
+        }
+        public string DatabaseName { get; set; } = "AppTracing";
 
+        public string TableName { get; set; } = "tracing_logs";
+
+        #endregion
         public DbTraceListener() { }
         public DbTraceListener(string xmlDocPath)
         {
@@ -36,14 +62,14 @@ namespace Tracing
             if (readCounter < 1)
                 throw new XmlException("Could not read xml file");
             databaseWriter = new DatabaseHandling.DatabaseWriter(connectionString);
-            if (!databaseWriter.TableExists(tableName))
+            if (!databaseWriter.TableExists(TableName))
             {
-                throw new NotSupportedException($"No table {tableName} in database.");
+                throw new NotSupportedException($"No table {TableName} in database.");
             }
                 
             foreach(var kv in acceptedFields)
             {
-                if(!databaseWriter.ColumnExists(dbName, tableName, kv.Value))
+                if(!databaseWriter.ColumnExists(DatabaseName, TableName, kv.Value))
                 {
                     Console.WriteLine(kv.Value);
                     throw new NotSupportedException($"No column {kv.Value} in database.");
@@ -58,7 +84,7 @@ namespace Tracing
 
         public override void WriteLine(string message)
         {
-            queriesBuilder.Append($"Insert into {tableName}({acceptedFields["messageField"]}, " +
+            queriesBuilder.Append($"Insert into {TableName}({acceptedFields["messageField"]}, " +
                 $"{acceptedFields["time"]}) " +
                 $"values('{message}', '{DateTime.Now}');");
             Console.WriteLine(queriesBuilder.ToString());
