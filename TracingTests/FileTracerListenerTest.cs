b043850c64ca57e542e0d58d8c8bce7ed50a0f62
Adrian Fijałkowski <fijaladrian@gmail.com>
2018-11-08 20:14:55 +0100

    moar tests nad small changes

---
+++ TracingTests/FileTracerListenerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tracing;
+
+namespace TracingTests
+{
+    [TestClass]
+    public class FileTracerListenerTest
+    {
+        [TestMethod]
+        public void FileExistsAfterTrace()
+        {
+            FileTraceListener ftl = new FileTraceListener("./test.txt");
+            ftl.Write("test");
+            ftl.Flush();
+            Assert.IsTrue(System.IO.File.Exists("./text.txt"));
+        }
+    }
+}
