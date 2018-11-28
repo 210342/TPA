3d8f6798a965c9bb6cb1111810c388fd009d62e7
Adrian Fijałkowski <fijaladrian@gmail.com>
2018-11-28 15:05:59 +0100

    missing ifles

---
+++ Serializing/IPersister.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializing
+{
+    public interface IPersister
+    {
+        void Save(object obj);
+        object Load(String path);
+    }
+}
---
+++ Serializing/XmlModelSerializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace Serializing
+{
+    [Export(typeof(IPersister))]
+    public class XmlModelSerializer : IPersister
+    {
+        public class item<KType, VType>
+        {
+            [XmlAttribute]
+            public KType id;
+            [XmlAttribute]
+            public VType value;
+        }
+        DataContractSerializer dcs; 
+        public XmlModelSerializer(List<Type> knownTypes, Type nodeType)
+        {
+            dcs = new DataContractSerializer(nodeType, knownTypes, 100000, false, true, null);
+        }
+        public void Save(object toSave)
+        {
+            var settings = new System.Xml.XmlWriterSettings { Indent = true };
+            using (var w = System.Xml.XmlWriter.Create("test.xml", settings))
+                dcs.WriteObject(w, toSave);
+        }
+        public object Read()
+        {
+            return null;
+        }
+
+        public object Load(string path)
+        {
+            object read = null;
+            using (var r = System.Xml.XmlReader.Create("test.xml"))
+                read = dcs.ReadObject(r);
+            return read;
+        }
+    }
+}
